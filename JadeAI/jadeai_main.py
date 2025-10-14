cbb251a69ddfdf4c365966f2089239bcfdd4a194
Richardt Simonsen <Richardtsimonsen@gmail.com>
2025-10-14 11:51:05 +0200

    Update jadeai_main.py
    This helps our JadeAI to connect with Vosksystem and Pyttsx3system

--- JadeAI/jadeai_main.py
+++ JadeAI/jadeai_main.py
@@ -1,35 +1,57 @@
-# Our main code for the AI
-
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
-import jadeai_instructions
+import sys
+import time
+import threading
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(BASE_DIR)
+
+sys.path.append("services_keys")
+sys.path.append("access_python")
+sys.path.append("calender")
+
+import vosksystem_python.speechtotext as stt
+
+thread = threading.Thread(target=stt.listen_loop, daemon=True)
+thread.start()
 
 load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-from jadeai_instructions import name
-from jadeai_instructions import personality
-from jadeai_instructions import demeanor
-from jadeai_instructions import tone
-from jadeai_instructions import formality
+from ai_python.jadeai_instructions import name, personality, demeanor, tone, formality
+
+from pyttsx3system_python.texttospeech import run_tts
 
 instructions = " ".join([name, personality, demeanor, tone, formality])
 
+print("[JADEAI SYSTEM] Online")
 
 while True:
-    user_input = input("Write your text here: ")
-    if user_input == "exit":
-        print("You have exited!")
-        break
-    
+    if stt.last_sentence:  
+        print(f"[JADEAI SYSTEM] Heard: {stt.last_sentence}")
+        
+        if stt.last_sentence.lower() == "exit":
+            print("[JADEAI SYSTEM] You have exited!")
+            break
 
-    response = client.responses.create(
-        model="gpt-4.1-mini",
-        input=[
-            {"role": "system", "content": instructions},
-            {"role": "user", "content": user_input}
-        ]
-    )
+        response = client.responses.create(
+            model="gpt-4.1",
+            input=[
+                {"role": "system", "content": instructions},
+                {"role": "user", "content": stt.last_sentence}
+            ],
+        )
 
-    print(response.output[0].content[0].text)
+        print("[JADEAI SYSTEM] Detected response")
+
+        stt.listening_enabled = False
+        run_tts(response)
+        time.sleep(2)
+        stt.rec = stt.reset_listener()
+        stt.listening_enabled = True
+
+        stt.last_sentence = ""  
+
+    time.sleep(1) 
